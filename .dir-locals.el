bbc1fb9b5cabdd597cdd7bfab9ef1b6da7c9a0ac
Brendon Walsh <brendonwalsh@niamu.com>
2022-07-04 21:28:11 -0400

    Figwheel + Cider workflow setup

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil
+  (cider-default-cljs-repl             . figwheel-main)
+  (cider-figwheel-main-default-options . "figwheel"))
+ (clojurescript-mode
+  (cider-clojure-cli-global-options    . "-A:figwheel")))
